c1b00db61132a4116da55c31a2ef6132688d903e
Anderson Augusto Rodrigo Silva <andersonaugustorodrigosilva@MacBook-Pro-de-Anderson.local>
2018-08-12 23:00:02 -0300

    Primeiro Comit

---
+++ src/pages/cadastrarQuestao.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page padding>
+   Cadasrtar Questao
+
+
+   <q-uploader
+  url="http://localhost:8070/api/questao/salvarImagemQuestao"
+  method="POST"
+  :headers="{ 'content-type': 'multipart/form-data' }"
+  @upload="postUpload"
+  :send-raw="true"
+  :no-content-type="true"
+/>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+  methods: {
+  postUpload (event) {
+    debugger
+    console.log(event) // event is just the file name! The full response would be more useful
+  },
+  async getSignedUrl (file) {
+    const contentType = file.type // To send the correct Content-Type
+    const fileName = file.name // If you want to use this value to calculate the S3 Key.
+    const response = await api.getSignedUrl({ fileName, contentType }) // Your api call to a sever that calculates the signed url.
+    return response.data.url
+  }
+}
+}
+</script>
+
+<style>
+</style>
