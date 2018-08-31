9b9fd9c4e5fe79fd97f57e481bd9efb3337f3860
Anderson Augusto Rodrigo Silva <andersonaugustorodrigosilva@MacBook-Pro-de-Anderson.local>
2018-08-30 23:50:23 -0300

    Teste remoto

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  URL_API: '"http://localhost:8070/api"',
+  URL_API: '"http://localhost:8080/ws-questionario/api"',
   VERSAO: '"0.5.0"'
 })
